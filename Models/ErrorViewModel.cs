d38e1b1e4bb56efe2ba888e65498f2b85a4e62be
MARTIN IOEL ZILBERSZTEIN <48520155@ort>
2024-05-30 14:07:17 -0300

    primerProyecto

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TP4_Zilbersztein_Entenza.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
